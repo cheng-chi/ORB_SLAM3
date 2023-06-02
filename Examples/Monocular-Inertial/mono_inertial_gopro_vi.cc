80c72a11e2e53fa59784755ed5effa77ce49eb76
Cheng Chi <cheng.chi@columbia.edu>
2023-06-02 16:50:05 -0400

    reduced not enough motion problem, fixed EuRoC output

--- Examples/Monocular-Inertial/mono_inertial_gopro_vi.cc
+++ Examples/Monocular-Inertial/mono_inertial_gopro_vi.cc
@@ -112,7 +112,7 @@ int main(int argc, char **argv) {
       }
 
 
-#ifdef COMPILEDWITHC11
+#ifdef COMPILEDWITHC14
       std::chrono::steady_clock::time_point t1 =
           std::chrono::steady_clock::now();
 #else
@@ -123,7 +123,7 @@ int main(int argc, char **argv) {
       // Pass the image to the SLAM system
       SLAM.TrackMonocular(im_track, tframe, vImuMeas);
 
-#ifdef COMPILEDWITHC11
+#ifdef COMPILEDWITHC14
       std::chrono::steady_clock::time_point t2 =
           std::chrono::steady_clock::now();
 #else
@@ -160,7 +160,9 @@ int main(int argc, char **argv) {
   cout << "mean tracking time: " << totaltime / nImages << endl;
 
   // Save camera trajectory
-  SLAM.SaveKeyFrameTrajectoryTUM("KeyFrameTrajectory.txt");
+  // SLAM.SaveKeyFrameTrajectoryTUM("KeyFrameTrajectory.txt");
+  SLAM.SaveTrajectoryEuRoC("CameraTrajectory.txt");
+  SLAM.SaveKeyFrameTrajectoryEuRoC("KeyFrameTrajectory.txt");
 
   return 0;
 }
