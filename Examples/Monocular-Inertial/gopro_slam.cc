0c60ab4e4a481c61fab07a6db756ee0c937ee3c3
WanCai-ac <837867565@qq.com>
2024-11-11 13:32:07 +0800

    Modified obtaining feature points outside the mask

--- Examples/Monocular-Inertial/gopro_slam.cc
+++ Examples/Monocular-Inertial/gopro_slam.cc
@@ -124,7 +124,7 @@ int main(int argc, char **argv) {
   std::string save_map;
   app.add_option("--save_map", save_map);
 
-  bool enable_gui = true;
+  bool enable_gui = false;
   app.add_flag("-g,--enable_gui", enable_gui);
 
   int num_threads = 4;
@@ -216,8 +216,7 @@ int main(int argc, char **argv) {
     bool success = cap.read(im);
     if (!success) {
       cout << "cap.read failed!" << endl;
-      //break;
-      continue;
+      break;
     }
 
     // resize image and draw gripper mask
